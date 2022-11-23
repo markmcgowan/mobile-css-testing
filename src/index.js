5562b7e830387ab1bdb1eda69cdbdfee7c3fb1d8
Mark McGowan <marktmcgowan55@gmail.com>
2022-11-23 15:22:02 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ document.getElementById("app").innerHTML = `
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here small change</a>.
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here small change a</a>.
 </div>
 `;
